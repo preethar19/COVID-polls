78f55b698a3c3c928f55f040afecac2ed25c3b7f
preetha <preeth8ram@gmail.com>
2020-04-22 00:01:39 +0000

    changed theme, fixed code style

--- datafest_covid/app.R
+++ datafest_covid/app.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(DT)
 library(readr)
 
+
 covid_approval_polls_adjusted <- read_csv("/cloud/project/covid_approval_polls_adjusted.csv")
 
 covid_approval_polls_adjusted <- covid_approval_polls_adjusted %>% 
@@ -32,50 +33,50 @@ party <- covid_approval_polls_adjusted %>%
 
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(theme = shinytheme("cosmo"),
-
-    # Application title
-    titlePanel("Trump Approval Ratings during COVID-19"),
-
-    # Sidebar
-    sidebarLayout(
-        sidebarPanel(
-            
-            dateRangeInput(inputId = "daterange",
-                           label   = "Enter poll date range",
-                           start   = "2020-02-02",
-                           end     = "2020-04-17",
-                           min     = "2020-02-02",
-                           max     = "2020-04-17"),
-            
-            radioButtons(inputId = "party",
-                         label = "Select party of interest",
-                         choices = party),
-            
-            selectInput(inputId = "pollster",
-                        label   = "Select pollster",
-                        choice  = pollsters),
-            
-            submitButton("Update View", icon("refresh")),
-            helpText("When you click the button above, you should see",
-                     "the output to the right update to reflect the values you",
-                     "entered at the top"),
-            verbatimTextOutput("value")
-            
-        ), # sidebarPanel
-        
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput(outputId = "plotpolls"),
-           dataTableOutput(outputId = "tablepolls")
-        )
-    )
+ui <- fluidPage(theme = shinytheme("united"),
+                
+                # Application title
+                titlePanel("Trump Approval Ratings during COVID-19"),
+                
+                # Sidebar
+                sidebarLayout(
+                    sidebarPanel(
+                        
+                        dateRangeInput(inputId = "daterange",
+                                       label   = "Enter poll date range",
+                                       start   = "2020-02-02",
+                                       end     = "2020-04-17",
+                                       min     = "2020-02-02",
+                                       max     = "2020-04-17"),
+                        
+                        radioButtons(inputId = "party",
+                                     label = "Select party of interest",
+                                     choices = party),
+                        
+                        selectInput(inputId = "pollster",
+                                    label   = "Select pollster",
+                                    choice  = pollsters),
+                        
+                        submitButton("Update View", icon("refresh")),
+                        helpText("When you click the button above, you should",
+                                 "see the output to the right update to",
+                                 "reflect the values you entered at the top"),
+                        verbatimTextOutput("value")
+                        
+                    ), # sidebarPanel
+                    
+                    
+                    # Show a plot of the generated distribution
+                    mainPanel(
+                        plotOutput(outputId = "plotpolls"),
+                        dataTableOutput(outputId = "tablepolls")
+                    )
+                )
 )
 
 # Define server logic 
 server <- function(input, output) {
-
+    
     output$plotpolls <- renderPlot({
         covid_approval_polls_adjusted %>% 
             filter(startdate >= input$daterange[1],
@@ -87,37 +88,37 @@ server <- function(input, output) {
             geom_line(color = "darkgreen", lty = 2) +
             geom_vline(color = "red", alpha = 0.4,
                        xintercept = as.numeric(as.Date("2020-02-24"))) +
-                annotate("text", x = as.Date("2020-02-24"), color = "red", alpha = 0.7, y = 10, 
+            annotate("text", x = as.Date("2020-02-24"), color = "red", alpha = 0.7, y = 10, 
                      label = "DOW J experiences\nworst day in\ntwo years") +
             geom_vline(color = "red", alpha = 0.4,
                        xintercept = as.numeric(as.Date("2020-03-06"))) +
-                annotate("text", x = as.Date("2020-03-06"), color = "red", alpha = 0.7, y = 60, 
-                         label = "$8.3 billion\nemergency\nspending package\nsigned") +
+            annotate("text", x = as.Date("2020-03-06"), color = "red", alpha = 0.7, y = 60, 
+                     label = "$8.3 billion\nemergency\nspending package\nsigned") +
             geom_vline(color = "red", alpha = 0.4,
                        xintercept = as.numeric(as.Date("2020-03-13"))) +
-                annotate("text", x = as.Date("2020-03-13"), color = "red", alpha = 0.7, y = 10, 
-                         label = "Trump declares\na national\nstate of\nemergency") +
+            annotate("text", x = as.Date("2020-03-13"), color = "red", alpha = 0.7, y = 10, 
+                     label = "Trump declares\na national\nstate of\nemergency") +
             geom_vline(color = "red", alpha = 0.4,
                        xintercept = as.numeric(as.Date("2020-03-18"))) +
-                annotate("text", x = as.Date("2020-03-18"), color = "red", alpha = 0.7, y = 80, 
-                         label = "Families First\nCoronavirus\nResponse Act\nsigned into law") +
+            annotate("text", x = as.Date("2020-03-18"), color = "red", alpha = 0.7, y = 80, 
+                     label = "Families First\nCoronavirus\nResponse Act\nsigned into law") +
             geom_vline(color = "red", alpha = 0.4,
                        xintercept = as.numeric(as.Date("2020-03-24"))) +
-                annotate("text", x = as.Date("2020-03-24"), color = "red", alpha = 0.7, y = 10, 
-                         label = "DOW J surges\nby more\nthan 2,000\npoints") +
+            annotate("text", x = as.Date("2020-03-24"), color = "red", alpha = 0.7, y = 10, 
+                     label = "DOW J surges\nby more\nthan 2,000\npoints") +
             geom_vline(color = "red", alpha = 0.4,
                        xintercept = as.numeric(as.Date("2020-03-27"))) +
-                annotate("text", x = as.Date("2020-03-27"), color = "red", alpha = 0.7, y = 60, 
-                         label = "Trump signs\n $2 trillion\ncoronavirus economic\nstimulus bill") +
+            annotate("text", x = as.Date("2020-03-27"), color = "red", alpha = 0.7, y = 60, 
+                     label = "Trump signs\n $2 trillion\ncoronavirus\neconomic\nstimulus bill") +
             geom_vline(color = "red", alpha = 0.4,
                        xintercept = as.numeric(as.Date("2020-04-11"))) +
-                annotate("text", x = as.Date("2020-04-11"), color = "red", alpha = 0.7, y = 10, 
-                         label = "US becomes\nthe worst-hit country\nin the world") +
+            annotate("text", x = as.Date("2020-04-11"), color = "red", alpha = 0.7, y = 10, 
+                     label = "US becomes\nthe worst-hit country\nin the world") +
             ylim(0,100) +
             xlim(as.Date("2020-02-02"), as.Date("2020-04-17")) +
             labs(x = "poll's starting date", y = "proportion approve") +
             theme_bw()
-
+        
     }) 
     
     output$tablepolls <- DT::renderDataTable({
@@ -126,10 +127,11 @@ server <- function(input, output) {
                    startdate <= input$daterange[2]) %>% 
             filter(pollster == input$pollster) %>% 
             filter(party == input$party) %>% 
-            select("party"|"startdate"|"enddate"|"pollster"|"samplesize"|"approve_adjusted"|"disapprove_adjusted")
+            select("party"|"startdate"|"enddate"|"pollster"|"samplesize"|
+                       "approve_adjusted"|"disapprove_adjusted")
     })
     
-
+    
 }
 
 ##no polls match criteria if selected attributes yield nothign
